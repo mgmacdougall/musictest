a86f1e57770098614fb0472953b9da0eab8dd342
Mike MacDougall <MacD>
2022-04-15 18:23:42 -0400

    Add of minor refactor to include only the name of the search string into the the getSong function

--- src/asyncindex.ts
+++ src/asyncindex.ts
@@ -1,42 +1,34 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import axios, { AxiosRequestConfig } from 'axios';
-// const API =process.env.APIKEY;
 
-async function getSong(){
-    
-    type SongDetails ={
-       'tracks':{
-           'hits':[]
-       },
-       'artists':{
-           'hits':[]
-       }
-    } 
-    
- try{
-     const options:AxiosRequestConfig = {
-      'method': 'GET',
-      'url': 'https://shazam.p.rapidapi.com/search',
-      'params': {'term':'kiss the sky' , 'locale': 'en-US', 'offset': '0', 'limit': '5'},
-      'headers': {
-        'X-RapidAPI-Host': 'shazam.p.rapidapi.com',
-        'X-RapidAPI-Key': `${process.env.APIKEY}`
-      }
+async function getSong(search:string) {
+
+    type SongDetails = {
+        tracks: {
+            hits: [];
+        };
+        artists: {
+            hits: [];
+        };
     };
 
-    const {data} = await axios.get<SongDetails>('https://shazam.p.rapidapi.com/search/', options)
-    return data;
+    try {
+        const options: AxiosRequestConfig = {
+            method: 'GET',
+            url: 'https://shazam.p.rapidapi.com/search',
+            params: { term: `${search}`, locale: 'en-US', offset: '0', limit: '5' },
+            headers: {
+                'X-RapidAPI-Host': 'shazam.p.rapidapi.com',
+                'X-RapidAPI-Key': `${process.env.APIKEY}`,
+            },
+        };
 
-
- }catch(error:any){
-    console.log(error)
- }
-    
-
+        const { data } = await axios.get<SongDetails>('https://shazam.p.rapidapi.com/search/', options);
+        return data;
+    } catch (error: unknown) {
+        console.log("Error during search")
+    }
 }
 
 // getSong();
-export {getSong}
-
-
-
+export { getSong };
