4d3b74e4ab6e1abd043089f0574f8ef7b60f9f26
Mike MacDougall <MacD>
2022-04-15 18:21:58 -0400

    Add of music tests to include the not null '!' and refactor to accept only string into asyncindex.ts file

--- src/tests/asyncindex.spec.ts
+++ src/tests/asyncindex.spec.ts
@@ -1,10 +1,20 @@
-import {getSong} from '../asyncindex';
+import { getSong } from '../asyncindex';
 
+const TESTSTRING = 'kiss the sky';
+describe('Song Index Test', () => {
 
-describe('Test', ()=>{
-  it('Test', async()=>{
-    const res = await getSong();
-    console.log(res?.artists.hits)
-      expect(res?.artists.hits.length).toEqual(5)
-  })
-})
+  it('Should return "4" artists when "kiss the sky" value used', async () => {
+      const res = await getSong(TESTSTRING);
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const num: number = res!.artists.hits!.length;
+      expect(num).toEqual(4);
+    });
+    
+    it('Should return "5" tracks when had coded value used.', async () => {
+      const res = await getSong(TESTSTRING);
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const num: number = res!.tracks.hits!.length;
+      expect(num).toEqual(5);
+  });
+  
+});
