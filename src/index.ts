f89a3137705e6fe9eedf28d63379328ce5399584
Mike MacDougall <MacD>
2022-04-15 18:20:29 -0400

    Add of express setup for the api bridge to music site

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,12 @@
-import axios, {AxiosRequestConfig } from 'axios';
+import express, {Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
-async function getSong(){
+const app:Express = express();
+const PORT = process.env.SERVERPORT;
 
- type SongDetails ={
-    'tracks':{
-        'hits':[]
-    },
-    'artists':{
-        'hits':[]
-    }
- } 
+app.get('/', (req:Request, res:Response)=>{
+    res.send('Hi there from Server!')
+})
 
- type GetSongDetailsResponse=
- {
-     data: SongDetails;
- }
-
- try{
-     const options:AxiosRequestConfig = {
-      'method': 'GET',
-      'url': 'https://shazam.p.rapidapi.com/search',
-      'params': {'term':'kiss the sky' , 'locale': 'en-US', 'offset': '0', 'limit': '5'},
-      'headers': {
-        'X-RapidAPI-Host': 'shazam.p.rapidapi.com',
-        'X-RapidAPI-Key': 'ef06e7a069msh025c9e3833f9827p193ea9jsna8c2a39ee783'
-      }
-    };
-
-    const test = await axios.get<GetSongDetailsResponse>('https://shazam.p.rapidapi.com/search/', options)
-    console.log(test)
-
-
- }catch(error:any){
-    console.log(error)
- }
-    
-
-}
-
-getSong();
-// export default {getSong}
-
-
-
+app.listen(PORT, ()=>console.log(`⚡️[server]: Server is running at https://localhost:${PORT}` ))
